fc2d240c3d7bbcbf33c7b6d0c674e06fa710c23e
Ionescu Paul-Andrei <70650460+R3dPraiseTheSun@users.noreply.github.com>
2022-03-23 13:55:13 +0200

    Added python server
    - added python server
    - added web folder where the webapp is places now

---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import multiprocessing
+from threading import Thread
+
+def task():
+	request = connection.recv(1024).decode('utf-8')
+	string_list = request.split(' ')     # Split request from spaces
+
+	method = string_list[0]
+	requesting_file = string_list[1]
+
+	print('Client request ',requesting_file)
+
+	myfile = requesting_file.split('?')[0] # After the "?" symbol not relevent here
+	myfile = myfile.lstrip('/')
+	if(myfile == 'web/' or myfile == ''):
+		myfile = 'web/index.html'    # Load index file as default
+
+	try:
+		file = open(myfile,'rb') # open file , r => read , b => byte format
+		response = file.read()
+		file.close()
+
+		header = 'HTTP/1.1 200 OK\n'
+
+		if(myfile.endswith(".jpg")):
+			mimetype = 'image/jpg'
+		elif(myfile.endswith(".css")):
+			mimetype = 'text/css'
+		elif(myfile.endswith(".js")):
+			mimetype = 'application/javascript'
+		else:
+			mimetype = 'text/html'
+
+		header += 'Content-Type: '+str(mimetype)+'\n\n'
+
+	except Exception as e:
+		header = 'HTTP/1.1 404 Not Found\n\n'
+		response = '<html><body><center><h3>Error 404: File not found</h3><p>Python HTTP Server</p></center></body></html>'.encode('utf-8')
+
+	final_response = header.encode('utf-8')
+	final_response += response
+	connection.send(final_response)
+	connection.close()
+
+HOST,PORT = '192.168.0.107',80
+
+my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+my_socket.bind((HOST,PORT))
+my_socket.listen(1)
+
+print('Serving on port ',PORT)
+
+while True:
+	connection,address = my_socket.accept()
+	threads = [Thread(target=task)]
+	for thread in threads:
+		thread.start()
+	for thread in threads:
+		thread.join()
+	
