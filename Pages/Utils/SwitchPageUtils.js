660b271efb8dbcebc76e4b845a08b810d0aeb8ae
Ionescu Paul-Andrei <70650460+R3dPraiseTheSun@users.noreply.github.com>
2022-03-23 13:50:46 +0200

    Finished Modularization
    - added Article const for every js page
    - added Footer.js with footer data
    - added Header.js with header data
    - changed every js page to accommodate newly added modals
    - changed export type to default inside js pages
    - created custom URL pages for every tab you can access

--- Pages/Utils/SwitchPageUtils.js
+++ Pages/Utils/SwitchPageUtils.js
@@ -1,5 +1,45 @@
 import  {pageMap}  from "../../render.js";
-export function changePage(number){ 
+
+let baseURL = document.URL;
+baseURL = baseURL.split('#')[0];
+let pageNUmber = 0;
+
+function processURL(response, urlPath){
+    document.title = response.title;
     var root = document.getElementById("root");
-    root.innerHTML = pageMap.get(number);
+    root.innerHTML = response.html;
+    document.location.href = baseURL + urlPath;
+}
+
+export function changePage(number){ 
+    var title;
+    var urlPath;
+    pageNUmber = number;
+    switch (number) {
+        case 0:
+            title = "Acasa";
+            urlPath = "#Acasa";
+            break;
+    
+        case 1:
+            title = 'Curse';
+            urlPath = "#Curse";
+            break;
+        case 2:
+            title = 'Adauga Bani';
+            urlPath = "#Adauga Bani";
+            break;
+        case 3:
+            title = 'About';
+            urlPath = "#About";
+            break;
+        default:
+            title = '404 not found';
+            urlPath = '#nuuuu mai avem';
+    }
+    let response = {
+        html: pageMap.get(number),
+        title: title
+    };
+    processURL(response, urlPath);
 }
