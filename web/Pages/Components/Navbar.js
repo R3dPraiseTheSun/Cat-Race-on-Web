d73493cbbadc42a2d0d273ecb2afdfa7a0b09333
Darth Wick <59597571+Darth-Wick@users.noreply.github.com>
2022-04-14 21:01:20 +0300

    3

--- web/Pages/Components/Navbar.js
+++ web/Pages/Components/Navbar.js
@@ -4,15 +4,12 @@ window.changePage=Utils.changePage;
 //define the modal
 let navigation=`
     <nav>
-    <div id="funnyCat">
-        <img src="https://external-preview.redd.it/H36GE4zP5H-ySwKN6opCb2OOWJFSMK2p2w1oLj1D4rc.jpg?auto=webp&s=215aa001c50138206c8d10b7d7712f30f951474c" alt="gambling cat owo">
-    </div>
-    <ul>
-        <li><button onclick="window.changePage(0)">Acasa</button></li>
-        <li><button onclick="window.changePage(1)">Curse</button></li>
-        <li><button onclick="window.changePage(2)">Adauga Bani</button></li>
-        <li><button onclick="window.changePage(3)">About</button></li>
-    </ul>
+        <button onclick="window.changePage(0)"><img id="logoCat" src="resources/logoCat.png"/></button>
+        <ul>
+            <li><button onclick="window.changePage(1)">Curse</button></li>
+            <li><button onclick="window.changePage(2)">Adauga Bani</button></li>
+            <li><button onclick="window.changePage(3)">About</button></li>
+        </ul>
     </nav>
 `;
 
