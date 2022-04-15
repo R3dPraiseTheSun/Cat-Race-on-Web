193fd996d68743b2e2515717841b1eb09a34ebf7
Darth Wick <59597571+Darth-Wick@users.noreply.github.com>
2022-04-15 20:41:27 +0300

    Finished About + Worked at Financiar

--- web/Pages/About/About.js
+++ web/Pages/About/About.js
@@ -7,15 +7,23 @@ const Article =`
     <div style="position: absolute;">
     </div>
     <div id="about">
-    <p>&emsp; Sa se implementeze o aplicatie Web oferind utilizatorilor autentificati posibilitatea realizarii de pariuri referitoare la cursele de feline.</p>
+    <p>Sa se implementeze o aplicatie Web oferind utilizatorilor autentificati posibilitatea realizarii de pariuri referitoare la cursele de feline.</p>
     <p>&nbsp;</p>
-    <p>&emsp; Sistemul va permite vizualizarea unui istoric al pariurilor si al rezultatelor aferente pentru fiecare concurent (pisica, motan).</p>
+    <p>Sistemul va permite vizualizarea unui istoric al pariurilor si al rezultatelor aferente pentru fiecare concurent (pisica, motan).</p>
     <p>&nbsp;</p>
-    <p>&emsp; Se va genera, de asemenea, atat un raport -- minimal, in formatele CSV, HTML, Markdown si XML -- oferind starea actuala a pariurilor, cat si calendarul (disponibil in formatul iCalendar) despre desfasurarea curselor din viitorul apropiat.</p>
+    <p>Se va genera, de asemenea, atat un raport -- minimal, in formatele CSV, HTML, Markdown si XML -- oferind starea actuala a pariurilor, cat si calendarul (disponibil in formatul iCalendar) despre desfasurarea curselor din viitorul apropiat.</p>
     <p>&nbsp;</p>
-    <p>&emsp; Pentru fiecare cursa, va exista un termen-limita de realizare a pariurilor.</p> 
+    <p>Pentru fiecare cursa, va exista un termen-limita de realizare a pariurilor.</p> 
     <p>&nbsp;</p>
-    <p>&emsp; Bonus: simularea serviciului de plata electronica vizand pariurile efectuate.</p>
+    <p>Bonus: simularea serviciului de plata electronica vizand pariurile efectuate.</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p><strong>Authors:</strong></p>
+    <p>Paul Ionescu</p>
+    <p>Cosmin Avramescu</p>
+    <p>&nbsp;</p>
+    <p><strong>Teacher:</strong></p>
+    <p>Amariei Ciprian</p>
     </div>
 </article>
 `
--- web/Pages/Financiar/Financiar.js
+++ web/Pages/Financiar/Financiar.js
@@ -4,9 +4,14 @@ import {Footer} from "../Components/Footer.js"
 
 const Article = `
 <article>
-    <div>
-        <img src="https://i.pinimg.com/736x/99/45/c8/9945c8754fcd23c0d502ee43e9d37f29.jpg" alt="Sold cat"/>
-        <audio src="resources/wideputinsong.mp3" type="audio/mpeg" controls></audio>
+    <div id=financiar>
+    <h1>Balanta</h1>
+    </div>
+    <div id=financiar>
+    <h1>Adauga Bani</h1>
+    </div>
+    <div id=financiar>
+    <h1>Istoric curse</h1>
     </div>
 </article>
 `;
