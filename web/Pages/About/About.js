7b5f807503b533148d5786e5a1509bffbd9bcb18
Darth Wick <59597571+Darth-Wick@users.noreply.github.com>
2022-04-15 20:00:44 +0300

    Remade About

--- web/Pages/About/About.js
+++ web/Pages/About/About.js
@@ -5,11 +5,17 @@ import {Footer} from "../Components/Footer.js"
 const Article =`
 <article>
     <div style="position: absolute;">
-        <h1>It's about drive</h1>
     </div>
-    <div>
-        <img src="https://i.guim.co.uk/img/media/c5e73ed8e8325d7e79babf8f1ebbd9adc0d95409/2_5_1754_1053/master/1754.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=065f279099ded1062688e357b155dc29" alt="About cat"/>
-        <audio src="resources/wideputinsong.mp3" type="audio/mpeg" controls></audio>
+    <div id="about">
+    <p>&emsp; Sa se implementeze o aplicatie Web oferind utilizatorilor autentificati posibilitatea realizarii de pariuri referitoare la cursele de feline.</p>
+    <p>&nbsp;</p>
+    <p>&emsp; Sistemul va permite vizualizarea unui istoric al pariurilor si al rezultatelor aferente pentru fiecare concurent (pisica, motan).</p>
+    <p>&nbsp;</p>
+    <p>&emsp; Se va genera, de asemenea, atat un raport -- minimal, in formatele CSV, HTML, Markdown si XML -- oferind starea actuala a pariurilor, cat si calendarul (disponibil in formatul iCalendar) despre desfasurarea curselor din viitorul apropiat.</p>
+    <p>&nbsp;</p>
+    <p>&emsp; Pentru fiecare cursa, va exista un termen-limita de realizare a pariurilor.</p> 
+    <p>&nbsp;</p>
+    <p>&emsp; Bonus: simularea serviciului de plata electronica vizand pariurile efectuate.</p>
     </div>
 </article>
 `
