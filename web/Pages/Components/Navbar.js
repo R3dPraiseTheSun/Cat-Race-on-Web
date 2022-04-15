a1f303f30aea68dd82da32790a3e27cd5a3509c9
Darth Wick <59597571+Darth-Wick@users.noreply.github.com>
2022-04-15 17:35:12 +0300

    4-More GUI changes

--- web/Pages/Components/Navbar.js
+++ web/Pages/Components/Navbar.js
@@ -1,3 +1,4 @@
+//Navbar.js
 import * as Utils from "../Utils/SwitchPageUtils.js"
 window.changePage=Utils.changePage;
 
@@ -5,6 +6,10 @@ window.changePage=Utils.changePage;
 let navigation=`
     <nav>
         <button onclick="window.changePage(0)"><img id="logoCat" src="resources/logoCat.png"/></button>
+        <div class="account-buttons">
+            <p id="login"><button onclick="window.showModalL()" id="loginBtn">Log in</button></p>
+            <p id="signup"><button onclick="window.showModalR()" id="signupBtn">Sign up</button></p>
+        </div>
         <ul>
             <li><button onclick="window.changePage(1)">Curse</button></li>
             <li><button onclick="window.changePage(2)">Financiar</button></li>
